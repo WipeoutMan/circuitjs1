0a140e4a28fcea576903a401f885d21dd5bfe85d
WipeoutMan <dgp8.007@gmail.com>
2026-05-07 16:17:04 +0200

    Revert "3rd commit"
    This reverts commit b7eea5ec16f0104fd6a36a5999c85907fe354362.

--- src/com/lushprojects/circuitjs1/client/ACRailElm.java
+++ src/com/lushprojects/circuitjs1/client/ACRailElm.java
@@ -1,7 +1,7 @@
 /*    
     Copyright (C) Paul Falstad and Iain Sharp
     
-    This file is part of CircuitJS1. test new
+    This file is part of CircuitJS1.
 
     CircuitJS1 is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
